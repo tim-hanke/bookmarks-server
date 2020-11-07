e52064261be5572a0b8563a6de5eab65af003611
Timothy Hanke <tim.hanke@gmail.com>
2020-11-06 19:31:39 -0500

    moved errorHandler and validateBearerToken to separate files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const validateBearerToken = require("./validate-bearer-token");
 const bookmarksRouter = require("./bookmarks/bookmarks-router");
+const errorHandler = require("./error-handler");
 
 const app = express();
 
@@ -14,28 +15,10 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get("Authorization");
-
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: "Unauthorized request" });
-  }
-  next();
-});
+app.use(validateBearerToken);
 
 app.use(bookmarksRouter);
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
